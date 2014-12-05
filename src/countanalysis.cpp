3c0dcdd37887e29abd84a8d44f5a364203e27f8c
Fabien Reumont-Locke <fabien.reumont-locke@polymtl.ca>
2014-12-05 14:41:28 -0500

    Added copyright + license info

--- src/countanalysis.cpp
+++ src/countanalysis.cpp
@@ -1,3 +1,21 @@
+/* Copyright (c) 2014 Fabien Reumont-Locke <fabien.reumont-locke@polymtl.ca>
+ *
+ * This file is part of lttng-parallel-analyses.
+ *
+ * lttng-parallel-analyses is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lttng-parallel-analyses is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lttng-parallel-analyses.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "countanalysis.h"
 
 #include <QList>
